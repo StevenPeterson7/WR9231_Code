d240385a7e022c2cc4b6a259ce156df36059e580
WHSRobotics10 <wilsonhighschoolrobotics@gmail.com>
2016-11-22 17:05:46 -0800

    Color Sensor stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTestOp.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+/**
+ * Created by phanau on 1/22/16.
+ * Test hardware color sensor
+ */
+@Autonomous(name="Test: MR Color Sensor Test 1", group ="Test")
+//@Disabled
+public class ColorSensorTestOp extends OpMode {
+
+    private ModernRoboticsI2cColorSensor mColorSensor;
+
+    public ColorSensorTestOp() {
+    }
+
+    public void init() {
+        // get hardware gyro
+        mColorSensor = (ModernRoboticsI2cColorSensor) hardwareMap.colorSensor.get("cs");
+    }
+
+    public void loop() {
+        telemetry.addData("red: ", mColorSensor.red());
+        telemetry.addData("green: ", mColorSensor.green());
+        telemetry.addData("blue: ", mColorSensor.blue());
+    }
+
+    public void stop() {}
+
+}
